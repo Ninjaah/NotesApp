7c1e77c2a3a74f3f3ff5377ac795bda643b1feeb
Ninjaah <ninja.luotonen@gmail.com>
2019-12-31 17:08:13 +0200

    Added basic styling

--- src/main/java/app/AppController.java
+++ src/main/java/app/AppController.java
@@ -15,11 +15,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class AppController {
 
 	@Autowired
-	private ProductService service; 
+	private ProductService noteService; 
+	@Autowired
+	private UserService userService; 
 	
 	@RequestMapping("/")
 	public String viewHomePage(Model model) {
-		List<Product> listProducts = service.listAll();
+		List<Product> listProducts = noteService.listAll();
 		model.addAttribute("listProducts", listProducts);
 		
 		return "index";
@@ -35,7 +37,7 @@ public class AppController {
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveProduct(@ModelAttribute("product") Product product) {
-		service.save(product);
+		noteService.save(product);
 		
 		return "redirect:/";
 	}
@@ -43,7 +45,7 @@ public class AppController {
 	@RequestMapping("/edit/{id}")
 	public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
 		ModelAndView mav = new ModelAndView("edit_note");
-		Product product = service.get(id);
+		Product product = noteService.get(id);
 		mav.addObject("product", product);
 		
 		return mav;
@@ -51,7 +53,22 @@ public class AppController {
 	
 	@RequestMapping("/delete/{id}")
 	public String deleteProduct(@PathVariable(name = "id") int id) {
-		service.delete(id);
+		noteService.delete(id);
 		return "redirect:/";		
 	}
+	
+	@RequestMapping("/login")
+	public String showLogInPage(Model model) {
+		User user = new User();
+		model.addAttribute("login", user); 
+
+		return "login";
+	}
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String saveUser(@ModelAttribute("user") User user) {
+		userService.save(user);
+
+		return "redirect:/";
+	}
+	
 }
